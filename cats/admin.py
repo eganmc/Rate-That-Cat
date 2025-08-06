725593dfe1940a6aa9fca7d6619787070857d732
Charles <eganmc@outlook.com>
2025-08-06 10:31:46 +0100

    added basic views, admin functionality and forms.

--- cats/admin.py
+++ cats/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Cat, Comment
 
 # Register your models here.
+admin.site.register(Cat)
+admin.site.register(Comment)
--- cats/views.py
+++ cats/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Cat
+from .forms import CatForm
 
 # Create your views here.
+
+def cat_list(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/cat_list.html', {'cats': cats})
+
+def cat_detail(request, pk):
+    cat = get_object_or_404(Cat, pk=pk)
+    return render(request, 'cats/cat_detail.html', {'cat': cat})
+
+def cat_create(request):
+    if request.method == 'POST':
+        form = CatForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('cat_list')
+    else:
+        form = CatForm()
+    return render(request, 'cats/cat_form.html', {'form': form})
+
+def cat_update(request, pk):
+    cat = get_object_or_404(Cat, pk=pk)
+    if request.method == 'POST':
+        form = CatForm(request.POST, request.FILES, instance=cat)
+        if form.is_valid():
+            form.save()
+            return redirect('cat_detail', pk=pk)
+    else:
+        form = CatForm(instance=cat)
+    return render(request, 'cats/cat_form.html', {'form': form})
+
+def cat_delete(request, pk):
+    cat = get_object_or_404(Cat, pk=pk)
+    if request.method == 'POST':
+        cat.delete()
+        return redirect('cat_list')
+    return render(request, 'cats/cat_confirm_delete.html', {'cat': cat})
