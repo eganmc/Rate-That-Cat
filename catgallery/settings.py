bc9abb963e7f636f282a89e9def5b5e8734277cd
Charles <eganmc@outlook.com>
2025-08-06 09:47:09 +0100

    added cats app and implemented into settings/urls

--- catgallery/settings.py
+++ catgallery/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cats',
 ]
+
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
