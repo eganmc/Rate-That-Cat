a52bc8a1f46fbc83d792f799eab70c4fcc03743d
Charles <210982295+eganmc@users.noreply.github.com>
2025-08-11 19:48:23 +0100

    fixed logout view

--- cats/urls.py
+++ cats/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('profile/', views.profile, name='profile'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('logout/', views.custom_logout, name='logout'),
 ]
